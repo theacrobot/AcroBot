e328e0fecaee1ed5dcf886a0942b920a151e34c3
Eliska Slobodova <eslobodo@redhat.com>
2013-04-07 00:12:11 +0200

    Categories and tags listing

--- AcroBot.rb
+++ AcroBot.rb
@@ -31,13 +31,27 @@ module AbbrevBot
   def lookup_dictionary(abbrev)
   	results = []
     dictionary.each do |key, values|
+    	next if values.nil?
       # next if key == 'new' # Skip items in the 'new' group
-      matched_keys = values.keys.select { |k| k.casecmp(abbrev) == 0 }
+      matched_keys = values.keys.select { |k| k.to_s.casecmp(abbrev) == 0 }
+      # => ['tbd', 'rhel'] ...
       matched_keys.each do |k|
       	results << [k, values[k]]
       end
     end
     results.empty? ? false : results 
+  end
+  
+  def find_values(tag)
+  	results =[]
+  	dictionary.each do |key, values|
+    	next if values.nil?
+      # next if key == 'new' # Skip items in the 'new' group
+      matched_keys = values.select { |k,v| v.to_s =~ /@#{tag}( |$)/i }.keys.each do |k|
+        results << k
+      end
+    end
+    results
   end
 
   def save_abbrev(name, description)
@@ -46,7 +60,7 @@ module AbbrevBot
     dict['new'][name.strip] = description.strip
     File.open('abbrev.yaml', 'w') { |f| YAML.dump(dict, f) }
   end
-
+  
 end
 
 include AbbrevBot
@@ -73,11 +87,29 @@ bot = Cinch::Bot.new do
 
   on :message, /^:help/i do |m|
     if m.channel?
-      m.reply("#{m.user.nick}: To expand an acronym, type e.g. *rhel")
-      m.reply("#{m.user.nick}: To add a new acronym, type e.g. *RHEL=Red Hat Enterprise Linux")
-    else # TODO: tyto retezce se opakuji, co by nemely
-      m.reply("To expand an acronym, type e.g. *rhel")
-      m.reply("To add a new acronym, type e.g. *rhel=Red Hat Enterprise Linux")
+      m.reply("#{m.user.nick}: To expand an acronym, type e.g. :rhel")
+      m.reply("#{m.user.nick}: To add a new acronym, type e.g. :RHEL=Red Hat Enterprise Linux")
+      m.reply("#{m.user.nick}: To list abbrevs associated with a tag, type eg. :@kernel")
+      m.reply("#{m.user.nick}: To list all tags, type :@tags")
+    else
+      m.reply("To expand an acronym, type e.g. :rhel")
+      m.reply("To add a new acronym, type e.g. :rhel=Red Hat Enterprise Linux")
+      m.reply("To list abbrevs associated with a tag, type e.g. :@kernel")
+      m.reply("To list all tags, type :@tags")
+    end
+  end
+  
+  on :message, /^:@([\w\-\_]+)$/ do |m, tag|
+  	tag = tag.strip
+  	match_abbrevs = find_values(tag)
+  	if match_abbrevs.empty?
+      m.reply("#{nick_str} Sorry, no such tag. To list all tags, type :@tags")
+    else
+      if m.channel?
+        m.reply("#{m.user.nick}: #{match_abbrevs.join(', ')}")
+      else
+        m.reply(match_abbrevs)
+      end
     end
   end
   
@@ -89,7 +121,7 @@ bot = Cinch::Bot.new do
       	replies.each do |original_abbrev, value|
       		reply_str = "%s %s stands for %s" % [
           	nick_str,
-          	Cinch::Formatting.format(:bold, original_abbrev),
+          	Cinch::Formatting.format(:bold, original_abbrev.to_s),
           	Cinch::Formatting.format(:bold, value)
           ]
           m.reply(reply_str.strip)
