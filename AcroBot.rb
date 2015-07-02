007fa548b3479e033dc06c78e670840a0cc9cb52
David O'Brien <davido@redhat.com>
2015-07-02 22:46:28 +1000

    Ported some dev changes from internal version.

--- AcroBot.rb
+++ AcroBot.rb
@@ -2,9 +2,6 @@ require 'rubygems'
 require 'cinch'
 require 'yaml'
 
-#comment goes here
-
-
 DEFAULT_DICTIONARY = {
   'new' => { 'tbd' => 'To be done.' }
 }
@@ -13,14 +10,21 @@ module AbbrevBot
 
   @@home_dir = File.expand_path '~/.config'
   @@install_dir = File.expand_path File.dirname(__FILE__)
-  @@abbrevs_file = "#{@@install_dir}/abbrev.yaml"
+  #@@abbrevs_file = "#{@@install_dir}/abbrev.yaml"
   @@cfg_file = 'acrobot.yaml'
 
-  def initialize_dictionary
-    File.open(@@abbrevs_file, 'w') { |f|
+# this bit won't work yet because we're not using @@abbrevs_file
+#  def initialize_dictionary
+#    File.open(@@abbrevs_file, 'w') { |f|
+#      YAML.dump(DEFAULT_DICTIONARY, f)
+#    }
+#    dictionary
+#  end
+  def initialize_dictionary(name)
+    File.open(name, 'w') { |f|
       YAML.dump(DEFAULT_DICTIONARY, f)
     }
-    dictionary
+    #dictionary
   end
 
   # Try to load the YAML file. If it does not exist initialize the
@@ -28,11 +32,59 @@ module AbbrevBot
   # If the file exists but is scrumbled (false), do the same thing.
   #
   def dictionary
-    begin
-      YAML.load_file(@@abbrevs_file) || initialize_dictionary
-    rescue Errno::ENOENT
-      initialize_dictionary
+      begin
+      h1 = YAML.load_file('public.yaml')
+      if h1 == false
+          puts("Initializing dictionary...")
+          initialize_dictionary('public.yaml')
+      h1 = YAML.load_file('public.yaml')
+      end
+      h2 = YAML.load_file('draft.yaml')
+      if h2 == false
+          puts("Initializaing dictionary...")
+          initialize_dictionary('draft.yaml')
+      h2 = YAML.load_file('draft.yaml')
+      end
+      h3 = YAML.load_file('slang.yaml')
+      if h3 == false
+          puts("Initializing dictionary...")
+          initialize_dictionary('slang.yaml')
+      h3 = YAML.load_file('slang.yaml')
+      end
+      h4 = YAML.load_file('internal.yaml')
+      if h4 == false
+          puts("Initializing dictionary...")
+          initialize_dictionary('internal.yaml')
+      h4 = YAML.load_file('internal.yaml')
+      end
+      h5 = YAML.load_file('tags.yaml')
+      if h5 == false
+          puts("Initializing dictionary...")
+          initialize_dictionary('tags.yaml')
+      h5 = YAML.load_file('tags.yaml')
+      end
+      # all tables should be merged
+      h1.merge(h2).merge(h3).merge(h4).merge(h5)
+      rescue Errno::ENOENT
+      initialize_dictionary('public.yaml')
+      initialize_dictionary('draft.yaml')
+      initialize_dictionary('slang.yaml')
+      initialize_dictionary('internal.yaml')
+      initialize_dictionary('tags.yaml')
+      h1 = YAML.load_file('public.yaml')
+      h2 = YAML.load_file('draft.yaml')
+      h3 = YAML.load_file('slang.yaml')
+      h4 = YAML.load_file('internal.yaml')
+      h5 = YAML.load_file('tags.yaml')
+      # all tables should be merged
+      h1.merge(h2).merge(h3).merge(h4).merge(h5)
     end
+
+#    begin
+#      YAML.load_file(@@abbrevs_file) || initialize_dictionary
+#    rescue Errno::ENOENT
+#      initialize_dictionary
+#    end
   end
 
   # Open the YAML file with dictionary and look for the abbrev
@@ -65,8 +117,8 @@ module AbbrevBot
   def save_abbrev(name, description)
     dict = dictionary
     dict['new'] ||= {}
-    dict['new'][name.strip] = description.strip
-    File.open(@@abbrevs_file, 'w') { |f| YAML.dump(dict, f) }
+    dict['new'][name.strip] = description.strip + ' @unchecked'
+    File.open('draft.yaml', 'w') { |f| YAML.dump(dict, f) }
   end
 
   def load_settings
@@ -88,8 +140,8 @@ bot = Cinch::Bot.new do
    c.realname = settings['realname'] # "IRC Acronym and Abbreviation Expander Bot. !help for help"
    c.user = settings['user']         # "Dacronym" #user name when connecting
    c.server = settings['server']     #"irc.freenode.net"
-   c.channels = settings['channels'] # ["#Dacronym","#katello","#openshift","#satellite6","#zanata","#theforeman"]
-#   c.channels = ["#Dacronym"]
+#   c.channels = settings['channels'] # ["#Dacronym","#katello","#openshift","#satellite6","#zanata","#theforeman"]
+   c.channels = ["#Dacronym"]
    c.prefix = settings['prefix']     # /^!/
   end
 
@@ -98,6 +150,7 @@ bot = Cinch::Bot.new do
     save_abbrev(abbrev, desc)
    nick = m.channel? ? m.user.nick+"":""
     m.reply("#{nick} Thanks! [#{abbrev}=#{desc}]")
+    m.reply("#{nick}++")
   end
 
   on :message, /^!help/i do |m|
