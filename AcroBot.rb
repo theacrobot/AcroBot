8f10d434acddc577a726c4c34c8a40aa980cc805
David O'Brien <daobrien@redhat.com>
2016-10-12 21:13:09 +1000

    Removed redundant dictionaries. Added one more channel.
    No need for different files for different types of acronyms. Merged into
    one.
    Added #ansible to list of channels.

--- AcroBot.rb
+++ AcroBot.rb
@@ -2,6 +2,9 @@ require 'rubygems'
 require 'cinch'
 require 'yaml'
 
+#comment goes here
+
+
 DEFAULT_DICTIONARY = {
   'new' => { 'tbd' => 'To be done.' }
 }
@@ -10,21 +13,14 @@ module AbbrevBot
 
   @@home_dir = File.expand_path '~/.config'
   @@install_dir = File.expand_path File.dirname(__FILE__)
-  #@@abbrevs_file = "#{@@install_dir}/abbrev.yaml"
+  @@abbrevs_file = "#{@@install_dir}/abbrev.yaml"
   @@cfg_file = 'acrobot.yaml'
 
-# this bit won't work yet because we're not using @@abbrevs_file
-#  def initialize_dictionary
-#    File.open(@@abbrevs_file, 'w') { |f|
-#      YAML.dump(DEFAULT_DICTIONARY, f)
-#    }
-#    dictionary
-#  end
-  def initialize_dictionary(name)
-    File.open(name, 'w') { |f|
+  def initialize_dictionary
+    File.open(@@abbrevs_file, 'w') { |f|
       YAML.dump(DEFAULT_DICTIONARY, f)
     }
-    #dictionary
+    dictionary
   end
 
   # Try to load the YAML file. If it does not exist initialize the
@@ -32,59 +28,11 @@ module AbbrevBot
   # If the file exists but is scrumbled (false), do the same thing.
   #
   def dictionary
-      begin
-      h1 = YAML.load_file('public.yaml')
-      if h1 == false
-          puts("Initializing dictionary...")
-          initialize_dictionary('public.yaml')
-      h1 = YAML.load_file('public.yaml')
-      end
-      h2 = YAML.load_file('draft.yaml')
-      if h2 == false
-          puts("Initializaing dictionary...")
-          initialize_dictionary('draft.yaml')
-      h2 = YAML.load_file('draft.yaml')
-      end
-      h3 = YAML.load_file('slang.yaml')
-      if h3 == false
-          puts("Initializing dictionary...")
-          initialize_dictionary('slang.yaml')
-      h3 = YAML.load_file('slang.yaml')
-      end
-      h4 = YAML.load_file('internal.yaml')
-      if h4 == false
-          puts("Initializing dictionary...")
-          initialize_dictionary('internal.yaml')
-      h4 = YAML.load_file('internal.yaml')
-      end
-      h5 = YAML.load_file('tags.yaml')
-      if h5 == false
-          puts("Initializing dictionary...")
-          initialize_dictionary('tags.yaml')
-      h5 = YAML.load_file('tags.yaml')
-      end
-      # all tables should be merged
-      h1.merge(h2).merge(h3).merge(h4).merge(h5)
-      rescue Errno::ENOENT
-      initialize_dictionary('public.yaml')
-      initialize_dictionary('draft.yaml')
-      initialize_dictionary('slang.yaml')
-      initialize_dictionary('internal.yaml')
-      initialize_dictionary('tags.yaml')
-      h1 = YAML.load_file('public.yaml')
-      h2 = YAML.load_file('draft.yaml')
-      h3 = YAML.load_file('slang.yaml')
-      h4 = YAML.load_file('internal.yaml')
-      h5 = YAML.load_file('tags.yaml')
-      # all tables should be merged
-      h1.merge(h2).merge(h3).merge(h4).merge(h5)
+    begin
+      YAML.load_file(@@abbrevs_file) || initialize_dictionary
+    rescue Errno::ENOENT
+      initialize_dictionary
     end
-
-#    begin
-#      YAML.load_file(@@abbrevs_file) || initialize_dictionary
-#    rescue Errno::ENOENT
-#      initialize_dictionary
-#    end
   end
 
   # Open the YAML file with dictionary and look for the abbrev
@@ -92,7 +40,6 @@ module AbbrevBot
     results = []
     dictionary.each do |key, values|
       next if values.nil?
-      # next if key == 'new' # Skip items in the 'new' group
       matched_keys = values.keys.select { |k| k.to_s.casecmp(abbrev) == 0 }
       # => ['tbd', 'rhel'] ...
       matched_keys.each do |k|
@@ -117,8 +64,8 @@ module AbbrevBot
   def save_abbrev(name, description)
     dict = dictionary
     dict['new'] ||= {}
-    dict['new'][name.strip] = description.strip + ' @unchecked'
-    File.open('draft.yaml', 'w') { |f| YAML.dump(dict, f) }
+    dict['new'][name.strip] = description.strip
+    File.open(@@abbrevs_file, 'w') { |f| YAML.dump(dict, f) }
   end
 
   def load_settings
@@ -136,12 +83,12 @@ bot = Cinch::Bot.new do
   settings = load_settings
 
   configure do |c|
-   c.nick = settings['nick']         # "Dacronym"
-   c.realname = settings['realname'] # "IRC Acronym and Abbreviation Expander Bot. !help for help"
-   c.user = settings['user']         # "Dacronym" #user name when connecting
-   c.server = settings['server']     #"irc.freenode.net"
-#   c.channels = settings['channels'] # ["#Dacronym","#katello","#openshift","#satellite6","#zanata","#theforeman"]
-   c.channels = ["#Dacronym"]
+   c.nick = settings['nick']         # "acrobot"
+   c.realname = settings['realname'] # "IRC Acronym and Abbreviation Expander Bot. '!help' for help"
+   c.user = settings['user']         # "acrobot" (user name when connecting)
+   c.server = settings['server']     # "irc.freenode.net"
+   c.channels = settings['channels'] # ["#katello","#openshift","#satellite6","#zanata","#theforeman","#ansible"]
+#   c.channels = ["#acrobot"]
    c.prefix = settings['prefix']     # /^!/
   end
 
@@ -150,7 +97,6 @@ bot = Cinch::Bot.new do
     save_abbrev(abbrev, desc)
    nick = m.channel? ? m.user.nick+"":""
     m.reply("#{nick} Thanks! [#{abbrev}=#{desc}]")
-    m.reply("#{nick}++")
   end
 
   on :message, /^!help/i do |m|
@@ -167,7 +113,7 @@ bot = Cinch::Bot.new do
     match_abbrevs = find_values(tag)
     nick = m.channel? ? m.user.nick+": " : ""
     if match_abbrevs.empty?
-      m.reply("#{nick}Sorry, no such tag. To list all tags, type !@tags")
+      m.reply("#{nick} Sorry, no such tag. To list all tags, type !@tags")
     else
         m.reply("#{nick}#{match_abbrevs.join(', ')}")
     end
@@ -180,7 +126,7 @@ bot = Cinch::Bot.new do
       if replies = lookup_dictionary(abbrev)
         replies.each do |original_abbrev, value|
           value, *tags = value.split('@')
-          reply_str = "%s %s stands for %s %s" % [
+          reply_str = "%s %s: %s %s" % [
             nick_str,
             Cinch::Formatting.format(:bold, original_abbrev.to_s),
             Cinch::Formatting.format(:bold, value.strip),
